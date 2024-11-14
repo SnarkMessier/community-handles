24e07c15389c651261f97e3d29b1b452a0b7a38f
SnarkMessier <snarkmessier@gmail.com>
2024-11-14 18:37:41 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
 import { NextResponse, type NextRequest } from "next/server"
-
 import { getDomain } from "./lib/utils"
 
 export function middleware(request: NextRequest) {
   const url = new URL(request.url)
-
   const { domain, subdomain } = getDomain(url.hostname)
 
-  if (domain) {
+  // Ensure domain is correctly recognized, modify condition as needed
+  if (domain === "nyr.social") {
     if (subdomain && subdomain !== process.env.LANDING_SUBDOMAIN) {
       return NextResponse.rewrite(
         new URL(`/${domain}/${subdomain}${url.pathname}${url.search}`, url)
@@ -18,18 +17,13 @@ export function middleware(request: NextRequest) {
       )
     }
   }
+
+  // Optional: Handle case where domain does not match expected value
+  return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. all root files inside /public (e.g. /favicon.ico)
-     * 4. opengraph images (e.g. /[a-z0-9-_.]/[a-z0-9-_]/opengraph-image)
-     * 5. Plausible analytics script
-     */
     "/((?!api/|_next/|_static/|js/|proxy/|[\\w-]+\\.\\w+|[a-zA-Z0-9-_.]+/[a-zA-Z0-9-_]+/opengraph-image).*)",
   ],
 }
